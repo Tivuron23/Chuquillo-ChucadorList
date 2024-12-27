2523434199f5bdfb83cf562c98886330ddcd70dc
Tivuron23 <168941653+Tivuron23@users.noreply.github.com>
2024-12-27 22:55:35 +0100

    Update score.js

--- js/score.js
+++ js/score.js
@@ -1,7 +1,7 @@
 /**
  * Numbers of decimal digits to round to
  */
-const scale = 3;
+const scale = 0;
 
 /**
  * Calculate the score awarded when having a certain percentage on a list level
@@ -11,10 +11,10 @@ const scale = 3;
  * @returns {Number}
  */
 export function score(rank, percent, minPercent) {
-    if (rank > 100) {
+    if (rank > 30) {
         return 0;
     }
-    if (rank > 200 && percent < 100) {
+    if (rank > 15 && percent < 100) {
         return 0;
     }
 
@@ -24,7 +24,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-24.9975*Math.pow(rank-1, 0.4) + 100) *
+    let score = (-24.9975*Math.pow(rank-1, 0.4) + 200) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
