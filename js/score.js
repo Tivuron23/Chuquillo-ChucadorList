6d7aebd9e763b04b3dff8bcd2e093d2d0c426592
Tivuron23 <168941653+Tivuron23@users.noreply.github.com>
2024-12-31 00:20:10 +0100

    Update score.js

--- js/score.js
+++ js/score.js
@@ -24,7 +24,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = 200 * 2.71**(-5.298 / (47) * (rank-1)) *
+    let score = 200 * 2.71**(-5.298 / (55) * (rank-1)) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
