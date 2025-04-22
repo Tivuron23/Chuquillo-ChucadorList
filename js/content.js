9f6c0e456bce8555039078cee61d0406347769ab
Tivuron23 <168941653+Tivuron23@users.noreply.github.com>
2025-04-22 22:24:45 +0200

    Update content.js

--- js/content.js
+++ js/content.js
@@ -68,9 +68,9 @@ export async function fetchLeaderboard() {
         };
         const { verified } = scoreMap[verifier];
         verified.push({
-            rank: rank,
+            rank: rank + 1,
             level: level.name,
-            score: score(rank, 100, level.percentToQualify),
+            score: score(rank + 1, 100, level.percentToQualify),
             link: level.verification,
         });
 
@@ -89,7 +89,7 @@ export async function fetchLeaderboard() {
                 completed.push({
                     rank: rank + 1,
                     level: level.name,
-                    score: score(rank, 100, level.percentToQualify),
+                    score: score(rank + 1, 100, level.percentToQualify),
                     link: record.link,
                 });
                 return;
