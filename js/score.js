0c0a93a137b44b812e44ba2cb8031e5d31c48243
Tivuron23 <168941653+Tivuron23@users.noreply.github.com>
2025-07-02 23:31:44 +0200

    Update score.js

--- js/score.js
+++ js/score.js
@@ -11,13 +11,20 @@ const scale = 3;
  * @returns {Number}
  */
 export function score(rank, percent, minPercent) {
-    if (rank > 44) {
+    if (rank > 51) {
         return 0;
     }
     if (rank > 18 && percent < 100) {
+        return 0;
+    }
+
+    // Old formula
+    /*
+    let score = (100 / Math.sqrt((rank - 1) / 50 + 0.444444) - 50) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
+    */
     // New formula
-    let score = 220 * 2.71**(-5.298 / (56) * (rank-1)) *
+    let score = 220 * 2.71**(-5.298 / (65) * (rank)) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
